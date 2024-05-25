1028d2990c44509f6c2b5b323dba17d25a424cf2
Daniels <d.jaunarajsjanvaris@gmail.com>
2024-05-25 17:21:29 +0300

    Izdarīts darba otrais punkts

---
+++ Assets/Script/ShowTextScrip.cs
@@ -0,0 +1,32 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowTextScript : MonoBehaviour
+{
+    public InputField nameInputField;
+    public InputField yearInputField;
+    public Text displayField;
+
+    string[] greetings = { "Sveiki ", "Jauku dienu ", "Labrīt ", "Prieks Tevi redzēt " };
+    
+    public void DisplayText()
+    {
+        string name = nameInputField.text;
+        string yearText = yearInputField.text;
+        
+        int year;
+        if (int.TryParse(yearText, out year))
+        {
+            int randomIndex = UnityEngine.Random.Range(0, greetings.Length);
+            int currentYear = DateTime.Now.Year;
+            int age = currentYear - year;
+
+            displayField.text = $"{greetings[randomIndex]}{name.ToLower()}! Supervaronis ir {age} gadus vecs!";
+        }
+        else
+        {
+            displayField.text = "Lūdzu, ievadiet pareizu dzimšanas gadu.";
+        }
+    }
+}
